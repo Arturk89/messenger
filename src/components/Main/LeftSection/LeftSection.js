9e9adc5df44572281b5eda7a7d21730b0f80f178
Artur Kempny <artur.kempny@gmail.com>
2021-03-17 11:44:50 +0100

    create empty main and sidebar component

---
+++ src/components/Main/LeftSection/LeftSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeftSection = () => {
+    return (
+        <div className="leftSection">
+            
+        </div>
+    )
+}
+
+export default LeftSection
---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import LeftSection from './LeftSection/LeftSection';
+import "./Main.css"
+
+const Main = () => {
+    return (
+        <div className="main">
+            <LeftSection />
+        </div>
+    )
+}
+
+export default Main
